269b47527af3ba2b4609a09303ed1d4a8627de6d
Mattew-Kentor <matveyzhikharev@yandex.ru>
2025-05-05 14:12:33 +0300

    fix tests

--- src/main/java/org/example/entity/BookEntity.java
+++ src/main/java/org/example/entity/BookEntity.java
@@ -1,6 +1,7 @@
 package org.example.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,6 +33,7 @@ public class BookEntity {
 
   @Schema(name = "Title", description = "Название книги")
   @Column(name = "title", length = 64)
+  @NotBlank
   private String title;
 
   @Schema(name = "AuthorId", description = "Автор")
--- src/main/java/org/example/entity/UserEntity.java
+++ src/main/java/org/example/entity/UserEntity.java
@@ -1,6 +1,7 @@
 package org.example.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -34,6 +36,7 @@ public class UserEntity {
 
   @Schema(name = "Name", description = "Имя")
   @Column(name = "name")
+  @NotBlank
   private String name;
 
   @Schema(name = "Surname", description = "Фамилия")
