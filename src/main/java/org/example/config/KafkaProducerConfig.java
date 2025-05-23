60627e4d00466b622ab258fa1e561c35aa5ce5ac
Mattew-Kentor <matveyzhikharev@yandex.ru>
2025-05-23 12:10:48 +0300

    tests

--- src/main/java/org/example/config/KafkaProducerConfig.java
+++ src/main/java/org/example/config/KafkaProducerConfig.java
@@ -1,9 +1,8 @@
 package org.example.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
